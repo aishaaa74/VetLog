c84c99213c88554068b1cc529a1af14f27b6ae9e
aishaaa74 <tbldva05@gmail.com>
2025-12-25 17:13:48 +0600

    fix  pet_pdf

--- vetlog_project/settings.py
+++ vetlog_project/settings.py
@@ -12,14 +12,11 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-0%x6%c#*u7$9tm1*!#)%cx31ihu$1f@si#i@qixpw1nvh5)e@-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
