9869af5533dcc07762f4766603659085bfa49827
aishaaa74 <tbldva05@gmail.com>
2025-12-25 17:14:24 +0600

    fix  urls Health

--- health/urls.py
+++ health/urls.py
@@ -5,7 +5,7 @@ app_name = 'health'
 
 urlpatterns = [
     path('', views.vaccination_list, name='list'),
-    path('create/', views.VaccinationCreateView.as_view(), name='create'),           # ← добавление
+    path('create/', views.VaccinationCreateView.as_view(), name='create'),          
     path('upcoming/', views.vaccination_upcoming, name='upcoming'),
     path('<int:pk>/update/', views.vaccination_update, name='update'),
     path('<int:pk>/delete/', views.vaccination_delete, name='delete'),
